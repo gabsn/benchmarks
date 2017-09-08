044e13690a0525683298d59907146e7574369368
gabsn <gabin.marignier@datadoghq.com>
2017-09-08 14:29:12 -0400

    Add concurrent benchmarks

--- buffers_test.go
+++ buffers_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"io"
+	"sync"
 	"testing"
 )
 
@@ -10,24 +11,20 @@ const MaxGoroutines = 10
 func BenchmarkOnePool(b *testing.B) {
 	pool := NewBufferPool()
 	for n := 0; n < b.N; n++ {
-		for i := 0; i < MaxGoroutines; i++ {
-			b := pool.Get()
-			b.Reset()
-			b.Write([]byte("Hello World!"))
-			pool.Put(b)
-		}
+		b := pool.Get()
+		b.Reset()
+		b.Write([]byte("Hello World!"))
+		pool.Put(b)
 	}
 }
 
 func BenchmarkOneChanPool(b *testing.B) {
 	pool := NewBufferChanPool()
 	for n := 0; n < b.N; n++ {
-		for i := 0; i < MaxGoroutines; i++ {
-			b := pool.Get()
-			b.Reset()
-			b.Write([]byte("Hello World!"))
-			pool.Put(b)
-		}
+		b := pool.Get()
+		b.Reset()
+		b.Write([]byte("Hello World!"))
+		pool.Put(b)
 	}
 }
 
@@ -35,16 +32,13 @@ func BenchmarkTwoPoolNoCopy(b *testing.B) {
 	pool1 := NewBufferPool()
 	pool2 := NewBufferPool()
 	for n := 0; n < b.N; n++ {
-		for i := 0; i < MaxGoroutines; i++ {
-			b1 := pool1.Get()
-			b2 := pool2.Get()
-			b1.Reset()
-			b2.Reset()
-			b1.Write([]byte("Hello World!"))
-			pool1.Put(b1)
-			pool2.Put(b2)
-		}
-
+		b1 := pool1.Get()
+		b2 := pool2.Get()
+		b1.Reset()
+		b2.Reset()
+		b1.Write([]byte("Hello World!"))
+		pool1.Put(b1)
+		pool2.Put(b2)
 	}
 }
 
@@ -52,16 +46,13 @@ func BenchmarkTwoChanPoolNoCopy(b *testing.B) {
 	pool1 := NewBufferChanPool()
 	pool2 := NewBufferChanPool()
 	for n := 0; n < b.N; n++ {
-		for i := 0; i < MaxGoroutines; i++ {
-			b1 := pool1.Get()
-			b2 := pool2.Get()
-			b1.Reset()
-			b2.Reset()
-			b1.Write([]byte("Hello World!"))
-			pool1.Put(b1)
-			pool2.Put(b2)
-		}
-
+		b1 := pool1.Get()
+		b2 := pool2.Get()
+		b1.Reset()
+		b2.Reset()
+		b1.Write([]byte("Hello World!"))
+		pool1.Put(b1)
+		pool2.Put(b2)
 	}
 }
 
@@ -69,17 +60,14 @@ func BenchmarkTwoPoolWriteTo(b *testing.B) {
 	pool1 := NewBufferPool()
 	pool2 := NewBufferPool()
 	for n := 0; n < b.N; n++ {
-		for i := 0; i < MaxGoroutines; i++ {
-			b1 := pool1.Get()
-			b2 := pool2.Get()
-			b1.Reset()
-			b2.Reset()
-			b1.Write([]byte("Hello World!"))
-			b1.WriteTo(b2)
-			pool1.Put(b1)
-			pool2.Put(b2)
-		}
-
+		b1 := pool1.Get()
+		b2 := pool2.Get()
+		b1.Reset()
+		b2.Reset()
+		b1.Write([]byte("Hello World!"))
+		b1.WriteTo(b2)
+		pool1.Put(b1)
+		pool2.Put(b2)
 	}
 }
 
@@ -87,17 +75,14 @@ func BenchmarkTwoChanPoolWriteTo(b *testing.B) {
 	pool1 := NewBufferChanPool()
 	pool2 := NewBufferChanPool()
 	for n := 0; n < b.N; n++ {
-		for i := 0; i < MaxGoroutines; i++ {
-			b1 := pool1.Get()
-			b2 := pool2.Get()
-			b1.Reset()
-			b2.Reset()
-			b1.Write([]byte("Hello World!"))
-			b1.WriteTo(b2)
-			pool1.Put(b1)
-			pool2.Put(b2)
-		}
-
+		b1 := pool1.Get()
+		b2 := pool2.Get()
+		b1.Reset()
+		b2.Reset()
+		b1.Write([]byte("Hello World!"))
+		b1.WriteTo(b2)
+		pool1.Put(b1)
+		pool2.Put(b2)
 	}
 }
 
@@ -105,17 +90,14 @@ func BenchmarkTwoPoolCopy(b *testing.B) {
 	pool1 := NewBufferPool()
 	pool2 := NewBufferPool()
 	for n := 0; n < b.N; n++ {
-		for i := 0; i < MaxGoroutines; i++ {
-			b1 := pool1.Get()
-			b2 := pool2.Get()
-			b1.Reset()
-			b2.Reset()
-			b1.Write([]byte("Hello World!"))
-			io.Copy(b2, b1)
-			pool1.Put(b1)
-			pool2.Put(b2)
-		}
-
+		b1 := pool1.Get()
+		b2 := pool2.Get()
+		b1.Reset()
+		b2.Reset()
+		b1.Write([]byte("Hello World!"))
+		io.Copy(b2, b1)
+		pool1.Put(b1)
+		pool2.Put(b2)
 	}
 }
 
@@ -123,17 +105,14 @@ func BenchmarkTwoChanPoolCopy(b *testing.B) {
 	pool1 := NewBufferChanPool()
 	pool2 := NewBufferChanPool()
 	for n := 0; n < b.N; n++ {
-		for i := 0; i < MaxGoroutines; i++ {
-			b1 := pool1.Get()
-			b2 := pool2.Get()
-			b1.Reset()
-			b2.Reset()
-			b1.Write([]byte("Hello World!"))
-			io.Copy(b2, b1)
-			pool1.Put(b1)
-			pool2.Put(b2)
-		}
-
+		b1 := pool1.Get()
+		b2 := pool2.Get()
+		b1.Reset()
+		b2.Reset()
+		b1.Write([]byte("Hello World!"))
+		io.Copy(b2, b1)
+		pool1.Put(b1)
+		pool2.Put(b2)
 	}
 }
 
@@ -142,17 +121,14 @@ func BenchmarkTwoPoolCopyBuffer(b *testing.B) {
 	pool2 := NewBufferPool()
 	buf := make([]byte, 8)
 	for n := 0; n < b.N; n++ {
-		for i := 0; i < MaxGoroutines; i++ {
-			b1 := pool1.Get()
-			b2 := pool2.Get()
-			b1.Reset()
-			b2.Reset()
-			b1.Write([]byte("Hello World!"))
-			io.CopyBuffer(b2, b1, buf)
-			pool1.Put(b1)
-			pool2.Put(b2)
-		}
-
+		b1 := pool1.Get()
+		b2 := pool2.Get()
+		b1.Reset()
+		b2.Reset()
+		b1.Write([]byte("Hello World!"))
+		io.CopyBuffer(b2, b1, buf)
+		pool1.Put(b1)
+		pool2.Put(b2)
 	}
 }
 
@@ -161,16 +137,233 @@ func BenchmarkTwoChanPoolCopyBuffer(b *testing.B) {
 	pool2 := NewBufferChanPool()
 	buf := make([]byte, 8)
 	for n := 0; n < b.N; n++ {
-		for i := 0; i < MaxGoroutines; i++ {
-			b1 := pool1.Get()
-			b2 := pool2.Get()
-			b1.Reset()
-			b2.Reset()
-			b1.Write([]byte("Hello World!"))
-			io.CopyBuffer(b2, b1, buf)
-			pool1.Put(b1)
-			pool2.Put(b2)
-		}
+		b1 := pool1.Get()
+		b2 := pool2.Get()
+		b1.Reset()
+		b2.Reset()
+		b1.Write([]byte("Hello World!"))
+		io.CopyBuffer(b2, b1, buf)
+		pool1.Put(b1)
+		pool2.Put(b2)
+	}
+}
 
+func BenchmarkConcurrentOnePool(b *testing.B) {
+	var wg sync.WaitGroup
+	pool := NewBufferPool()
+	for n := 0; n < b.N; n++ {
+		for i := 0; i < MaxGoroutines; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				b := pool.Get()
+				b.Reset()
+				b.Write([]byte("Hello World!"))
+				pool.Put(b)
+			}()
+		}
+		wg.Wait()
+	}
+}
+
+func BenchmarkConcurrentOneChanPool(b *testing.B) {
+	var wg sync.WaitGroup
+	pool := NewBufferChanPool()
+	for n := 0; n < b.N; n++ {
+		for i := 0; i < MaxGoroutines; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				b := pool.Get()
+				b.Reset()
+				b.Write([]byte("Hello World!"))
+				pool.Put(b)
+			}()
+		}
+		wg.Wait()
+	}
+}
+
+func BenchmarkConcurrentTwoPoolNoCopy(b *testing.B) {
+	var wg sync.WaitGroup
+	pool1 := NewBufferPool()
+	pool2 := NewBufferPool()
+	for n := 0; n < b.N; n++ {
+		for i := 0; i < MaxGoroutines; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				b1 := pool1.Get()
+				b2 := pool2.Get()
+				b1.Reset()
+				b2.Reset()
+				b1.Write([]byte("Hello World!"))
+				pool1.Put(b1)
+				pool2.Put(b2)
+			}()
+		}
+		wg.Wait()
+	}
+}
+
+func BenchmarkConcurrentTwoChanPoolNoCopy(b *testing.B) {
+	var wg sync.WaitGroup
+	pool1 := NewBufferChanPool()
+	pool2 := NewBufferChanPool()
+	for n := 0; n < b.N; n++ {
+		for i := 0; i < MaxGoroutines; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				b1 := pool1.Get()
+				b2 := pool2.Get()
+				b1.Reset()
+				b2.Reset()
+				b1.Write([]byte("Hello World!"))
+				pool1.Put(b1)
+				pool2.Put(b2)
+			}()
+		}
+		wg.Wait()
+	}
+}
+
+func BenchmarkConcurrentTwoPoolWriteTo(b *testing.B) {
+	var wg sync.WaitGroup
+	pool1 := NewBufferPool()
+	pool2 := NewBufferPool()
+	for n := 0; n < b.N; n++ {
+		for i := 0; i < MaxGoroutines; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				b1 := pool1.Get()
+				b2 := pool2.Get()
+				b1.Reset()
+				b2.Reset()
+				b1.Write([]byte("Hello World!"))
+				b1.WriteTo(b2)
+				pool1.Put(b1)
+				pool2.Put(b2)
+			}()
+		}
+		wg.Wait()
+	}
+}
+
+func BenchmarkConcurrentTwoChanPoolWriteTo(b *testing.B) {
+	var wg sync.WaitGroup
+	pool1 := NewBufferChanPool()
+	pool2 := NewBufferChanPool()
+	for n := 0; n < b.N; n++ {
+		for i := 0; i < MaxGoroutines; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				b1 := pool1.Get()
+				b2 := pool2.Get()
+				b1.Reset()
+				b2.Reset()
+				b1.Write([]byte("Hello World!"))
+				b1.WriteTo(b2)
+				pool1.Put(b1)
+				pool2.Put(b2)
+			}()
+		}
+		wg.Wait()
+	}
+}
+
+func BenchmarkConcurrentTwoPoolCopy(b *testing.B) {
+	var wg sync.WaitGroup
+	pool1 := NewBufferPool()
+	pool2 := NewBufferPool()
+	for n := 0; n < b.N; n++ {
+		for i := 0; i < MaxGoroutines; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				b1 := pool1.Get()
+				b2 := pool2.Get()
+				b1.Reset()
+				b2.Reset()
+				b1.Write([]byte("Hello World!"))
+				io.Copy(b2, b1)
+				pool1.Put(b1)
+				pool2.Put(b2)
+			}()
+		}
+		wg.Wait()
+	}
+}
+
+func BenchmarkConcurrentTwoChanPoolCopy(b *testing.B) {
+	var wg sync.WaitGroup
+	pool1 := NewBufferChanPool()
+	pool2 := NewBufferChanPool()
+	for n := 0; n < b.N; n++ {
+		for i := 0; i < MaxGoroutines; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				b1 := pool1.Get()
+				b2 := pool2.Get()
+				b1.Reset()
+				b2.Reset()
+				b1.Write([]byte("Hello World!"))
+				io.Copy(b2, b1)
+				pool1.Put(b1)
+				pool2.Put(b2)
+			}()
+		}
+		wg.Wait()
+	}
+}
+
+func BenchmarkConcurrentTwoPoolCopyBuffer(b *testing.B) {
+	var wg sync.WaitGroup
+	pool1 := NewBufferPool()
+	pool2 := NewBufferPool()
+	buf := make([]byte, 8)
+	for n := 0; n < b.N; n++ {
+		for i := 0; i < MaxGoroutines; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				b1 := pool1.Get()
+				b2 := pool2.Get()
+				b1.Reset()
+				b2.Reset()
+				b1.Write([]byte("Hello World!"))
+				io.CopyBuffer(b2, b1, buf)
+				pool1.Put(b1)
+				pool2.Put(b2)
+			}()
+		}
+		wg.Wait()
+	}
+}
+
+func BenchmarkConcurrentTwoChanPoolCopyBuffer(b *testing.B) {
+	var wg sync.WaitGroup
+	pool1 := NewBufferChanPool()
+	pool2 := NewBufferChanPool()
+	buf := make([]byte, 8)
+	for n := 0; n < b.N; n++ {
+		for i := 0; i < MaxGoroutines; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				b1 := pool1.Get()
+				b2 := pool2.Get()
+				b1.Reset()
+				b2.Reset()
+				b1.Write([]byte("Hello World!"))
+				io.CopyBuffer(b2, b1, buf)
+				pool1.Put(b1)
+				pool2.Put(b2)
+			}()
+		}
+		wg.Wait()
 	}
 }
